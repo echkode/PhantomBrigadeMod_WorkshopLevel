a4a33dd94e00feffd48c00b22058fe808cfb512f
EchKode <48565771+echkode@users.noreply.github.com>
2023-09-05 17:26:57 +0000

    Add code

---
+++ Code/ModLink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using HarmonyLib;
+using PBModManager = PhantomBrigade.Mods.ModManager;
+using UnityEngine;
+
+namespace EchKode.PBMods.WorkshopLevel
+{
+	public partial class ModLink : PhantomBrigade.Mods.ModLink
+	{
+		internal static int modIndex;
+		internal static string modID;
+		internal static string modPath;
+
+		public override void OnLoad(Harmony harmonyInstance)
+		{
+			// Uncomment to get a file on the desktop showing the IL of the patched methods.
+			// Output from FileLog.Log() will trigger the generation of that file regardless if this is set so
+			// FileLog.Log() should be put in a guard.
+			//Harmony.DEBUG = true;
+
+			modIndex = PBModManager.loadedMods.Count;
+			modID = metadata.id;
+			modPath = metadata.path;
+
+			var patchAssembly = typeof(ModLink).Assembly;
+			Debug.Log($"Mod {metadata.id} is executing OnLoad | Using HarmonyInstance.PatchAll on assembly ({patchAssembly.FullName}) | Directory: {metadata.directory} | Full path: {metadata.path}");
+			harmonyInstance.PatchAll(patchAssembly);
+
+			if (Harmony.DEBUG)
+			{
+				FileLog.Log($"{new string('=', 20)} Start [{DateTime.Now:u}] {new string('=', 20)}");
+				FileLog.Log("!!! PBMods patches applied");
+			}
+		}
+	}
+}
---
+++ Code/Patch.cs
@@ -0,0 +1,89 @@
+﻿using HarmonyLib;
+
+using PhantomBrigade;
+using PBOverworldBaseLevelSystem = PhantomBrigade.Overworld.Systems.OverworldBaseLevelSystem;
+
+using UnityEngine;
+
+namespace EchKode.PBMods.WorkshopLevel
+{
+	[HarmonyPatch]
+	static class Patch
+	{
+		[HarmonyPatch(typeof(PBOverworldBaseLevelSystem), "RefreshBaseLevels")]
+		[HarmonyPrefix]
+		static bool Obls_RefreshBaseLevels()
+		{
+			var persistent = Contexts.sharedInstance.persistent;
+			var currentLevel = persistent.hasBaseProvinceLevel ? persistent.baseProvinceLevel.level : 0;
+			var inventoryParts = EquipmentUtility.GetPartsInInventory(IDUtility.playerBasePersistent);
+			if (inventoryParts == null || inventoryParts.Count == 0)
+			{
+				return false;
+			}
+
+			var newLevel = currentLevel;
+			foreach (var part in inventoryParts )
+			{
+				// Get highest level from parts in the base's inventory.
+
+				if (part == null)
+				{
+					continue;
+				}
+				if (!part.hasLevel)
+				{
+					continue;
+				}
+				if (part.level.i <= newLevel)
+				{
+					continue;
+				}
+				newLevel = part.level.i;
+			}
+
+			foreach (var unit in Contexts.sharedInstance.persistent.GetEntitiesWithEntityLinkPersistentParent(IDUtility.playerBasePersistent.id.id))
+			{
+				// Parts loaded out on squad are not necessarily included in the base's inventory.
+
+				foreach (var part in EquipmentUtility.GetPartsInUnit(unit))
+				{
+					if (part == null)
+					{
+						continue;
+					}
+					if (!part.hasLevel)
+					{
+						continue;
+					}
+					if (part.level.i <= newLevel)
+					{
+						continue;
+					}
+					newLevel = part.level.i;
+				}
+			}
+
+			// If you want to adjust the level found, do the calculation here.
+			//newLevel = Mathf.RoundToInt(newLevel * 0.9f);
+
+			if (newLevel <= currentLevel)
+			{
+				// Workshop level only goes up.
+
+				return false;
+			}
+
+			Debug.LogFormat(
+				"Mod {0} ({1}) New base level: {0} based on highest part level in inventory or loaded out on squad | Previous base level: {1}",
+				ModLink.modIndex,
+				ModLink.modID,
+				newLevel,
+				currentLevel);
+			persistent.ReplaceBaseProvinceLevel(newLevel);
+			persistent.ReplaceWorkshopLevel(newLevel);
+
+			return false;
+		}
+	}
+}
